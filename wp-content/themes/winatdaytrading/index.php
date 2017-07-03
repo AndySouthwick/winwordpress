6db28b261d34cdde40fb64019a9ec1b71b8ca1a5
andySouthwick <asouthwick01@gmail.com>
2017-07-03 16:37:44 -0600

    looks good

--- wp-content/themes/winatdaytrading/index.php
+++ wp-content/themes/winatdaytrading/index.php
@@ -40,14 +40,14 @@ get_header(); ?>
     </div>
                     <div class="pagecontent">
                         <div class="fluid-container second-block">
-                            <div class="row justify-content-center"><h2>FAQ</h2></div><hr/>
+                            <div class="row justify-content-center"><h2>Frequently Asked Questions</h2></div><hr/>
                             <div class="row ">
                                 <div class="col-md-3 d-flex justify-content-center">
                                     
                                         <div class="card" style="width: 20rem;">
                                             <img class="card-img-top" src="wp-content/uploads/2017/06/AdobeStock_97903565-300x213.jpeg" alt="Card image cap" height="250">
                                         <div class="card-block">
-                                        <h4 class="card-title">Card title</h4>
+                                        <h4 class="card-title">Why Forex market</h4>
                                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-primary">Go somewhere</a>
                                         </div>
@@ -57,7 +57,7 @@ get_header(); ?>
                                         <div class="card" style="width: 20rem;">
                                             <img class="card-img-top" src="wp-content/uploads/2017/06/AdobeStock_88185558-300x200.jpeg" alt="Card image cap" height="250">
                                         <div class="card-block">
-                                        <h4 class="card-title">Card title</h4>
+                                        <h4 class="card-title">Why we let our students trade our money</h4>
                                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-primary">Go somewhere</a>
                                         </div>
@@ -67,7 +67,7 @@ get_header(); ?>
                                         <div class="card" style="width: 20rem;">
                                             <img class="card-img-top" src="wp-content/uploads/2017/06/AdobeStock_107551065-300x150.jpeg" alt="Card image cap" height="250">
                                         <div class="card-block">
-                                        <h4 class="card-title">Card title</h4>
+                                        <h4 class="card-title">Forex coaching</h4>
                                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-primary">Go somewhere</a>
                                         </div>
@@ -77,7 +77,7 @@ get_header(); ?>
                                         <div class="card" style="width: 20rem;">
                                             <img class="card-img-top" src="wp-content/uploads/2017/06/AdobeStock_76528287-300x237.jpeg" alt="Card image cap" height="250">
                                         <div class="card-block">
-                                        <h4 class="card-title">Card title</h4>
+                                        <h4 class="card-title">Can I trade full time</h4>
                                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-primary">Go somewhere</a>
                                         </div>
