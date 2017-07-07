1f6b1328f4e219fce8fa8258fa4344c441ef0ce5
andySouthwick <asouthwick01@gmail.com>
2017-07-07 16:20:39 -0600

    these are commits

--- wp-content/themes/winatdaytrading/page.php
+++ wp-content/themes/winatdaytrading/page.php
@@ -10,14 +10,14 @@ get_header();
 ?>
 
                     	
-                    	<div class="container">
+                    	<div class="container ">
                           
  <?php while ( have_posts() ) : the_post(); ?>
                     	<? the_title('<h3>', '</h3>'); ?>
                           <p> <?php  the_content(); ?></p>
                         <?php endwhile; ?>
 
-                        
+
                         </div>
                        
 
