fea594440fc1ded4387ac33276f23df72092e70d
andySouthwick <asouthwick01@gmail.com>
2017-07-11 09:19:53 -0600

    added margin top

--- wp-content/themes/winatdaytrading/page.php
+++ wp-content/themes/winatdaytrading/page.php
@@ -10,7 +10,7 @@ get_header();
 ?>
 
                     	
-                    	<div class="container contactconatain">
+                    	<div class="container contactconatain margintop">
                           
  <?php while ( have_posts() ) : the_post(); ?>
                           <p> <?php  the_content(); ?></p>
