78db35a012fb1bf31aaad231b1cee09e2e2605ef
andySouthwick <asouthwick01@gmail.com>
2017-07-19 07:11:53 -0600

    send email script

---
+++ send_email.php
@@ -0,0 +1,170 @@
+
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+     
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "kdeck6@aol.com";
+ 
+    $email_subject = "Tryout Form Submitted";
+ 
+     
+ 
+     
+ 
+ function died($error) {
+ 
+     //your error code can go here
+ 
+     echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+    // validation expected data exists
+ 
+  /*  if(!isset($_POST['age']) ||
+ 
+        !isset($_POST['fn']) ||
+ 
+        !isset($_POST['ln']) ||
+ 
+        !isset($_POST['pg']) ||
+ 
+        !isset($_POST['email']) ||
+		
+		 !isset($_POST['phone']) ||
+		 
+		  !isset($_POST['day']) ||
+		  
+		   !isset($_POST['month']) ||
+		   
+		    !isset($_POST['year']) ||
+			
+			 !isset($_POST['pos1']) ||
+			 
+			  !isset($_POST['pos2']) ||
+			  
+			   !isset($_POST['throws']) ||
+			   
+			    !isset($_POST['bats'])) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');      
+ 
+    }*/
+ 
+     
+
+	
+    $first_name = $_POST['fname']; // required
+ 
+    $last_name = $_POST['lname']; // required
+	
+ 	$email_from = $_POST['email']; // required
+ 
+    $phone = $_POST['phone']; // required
+ 
+   $message = $_POST['message']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+ 
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+ /* if(strlen($comments) < 2) {
+ 
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+ 
+  }*/
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+    
+	$email_message .= "First Name: ".clean_string($first_name)."\n";
+ 
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+	 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+	
+	$email_message .= "phone: ".($phone)."\n";
+ 
+    $email_message .= "message: ".clean_string($message)."\n";
+ 
+ 
+     
+ 
+     
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers); 
+ 
+?>
+ 
+ 
+
+ 
+ 
+<?php
+ 
+}
+ 
+?>
+<?php header("Location: http://winatdaytrading.com/thankyou") ?>
+ 
